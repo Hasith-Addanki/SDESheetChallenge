b9d3915b62f663f4a8e5739d7aa7fd5dcc3599a7
Hasith-Addanki <hasithaddanki@gmail.com>
2026-06-11 23:59:48 +0530

    #SDESheetChallenge - Day-11 2026-06-11

---
+++ Linked List/032_linked-list-cycle.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/linked-list-cycle/description/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while(slow && fast) {
+            slow = slow->next;
+            fast = fast->next;
+            if(fast == nullptr || slow == nullptr)
+                return false;
+            fast = fast->next;
+            if(slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+};
